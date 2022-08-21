3ce34f0308ac69ef350c77475d983504299b9697
Juan Rodriguez <nushio@gmail.com>
2022-08-21 13:55:21 -0500

    credits fixes

--- lib/widgets/credits_menu.dart
+++ lib/widgets/credits_menu.dart
@@ -18,100 +18,96 @@ class CreditsMenu extends StatelessWidget {
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
         child: SizedBox(
-          width: MediaQuery.of(context).size.width * 0.8,
-          height: MediaQuery.of(context).size.height * 0.8,
+          width: MediaQuery.of(context).size.width * 0.9,
+          height: MediaQuery.of(context).size.height * 0.9,
           child: Card(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(20),
             ),
             color: Colors.black.withAlpha(100),
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 20, horizontal: 100),
-              child: Column(
-                children: [
-                  Column(
-                    children: [
-                      Align(
-                        alignment: const AlignmentDirectional(0.85, 0),
-                        child: TextButton(
-                          onPressed: () {
-                            gameRef.overlays.remove(CreditsMenu.id);
-                            gameRef.overlays.add(MainMenu.id);
-                          },
-                          child: const Icon(Icons.close),
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    Align(
+                      alignment: const AlignmentDirectional(0.85, 0),
+                      child: TextButton(
+                        onPressed: () {
+                          gameRef.overlays.remove(CreditsMenu.id);
+                          gameRef.overlays.add(MainMenu.id);
+                        },
+                        child: const Icon(Icons.close),
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const [
+                      Text(
+                        'Credits',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
                         ),
+                      ),
+                      Text(
+                        'Graphics',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        '1-bit graphics by Kenney',
+                      ),
+                      Text(
+                        'Audio',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'Music: Red Skies by HoliznaCC0',
+                      ),
+                      Text(
+                        'SFX by phoenix1291',
+                      ),
+                      Text(
+                        'Coding',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'Nushio',
+                      ),
+                      Text(
+                        'Special Thanks',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'Flame Engine',
+                      ),
+                      Text(
+                        "Dev Kage's Flutter tutorials",
+                      ),
+                      Text(
+                        'Wife & Kid! <3',
+                      ),
+                      Text(
+                        'And YOU for playing!',
                       ),
                     ],
                   ),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: const [
-                        Text(
-                          'Credits',
-                          style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          'Graphics',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          '1-bit graphics by Kenney',
-                        ),
-                        Text(
-                          'Audio',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          'Music: Red Skies by HoliznaCC0',
-                        ),
-                        Text(
-                          'SFX by phoenix1291',
-                        ),
-                        Text(
-                          'Coding',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          'Nushio',
-                        ),
-                        Text(
-                          'Special Thanks',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          'Flame Engine',
-                        ),
-                        Text(
-                          "Dev Kage's Flutter tutorials",
-                        ),
-                        Text(
-                          'Wife & Kid! <3',
-                        ),
-                        Text(
-                          'And YOU for playing!',
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
